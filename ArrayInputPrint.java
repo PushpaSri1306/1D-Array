f2badb08c693ac12e8fc0ab8dc183e324d829ec1
PushpaSri1306 <puttapushpasri@gmail.com>
2025-01-28 21:52:20 +0530

    1D Array

---
+++ ArrayInputPrint.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ArrayInputPrint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Elements of the array:");
+        printArray(array);
+
+        scanner.close();
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
+
+
